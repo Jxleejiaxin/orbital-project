c2127cc8874a6fc5d7cb6dccf0bc2e91e790a54c
Lee Jia Xin <jxleejiaxin@gmail.com>
2022-05-26 21:58:32 +0800

    Whole folder for webapp. Yet to be integrated with telegram bot

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
-import {useState, useEffect} from "react";
-import './App.css';
-import Card from './components/Card/Card';
-import Cart from './components/Cart/Cart';
-const {getMenu} = require("./db/db");
+import { useState, useEffect } from "react";
+import "./App.css";
+import Card from "./components/Card/Card";
+import Cart from "./components/Cart/Cart";
+const { getMenu } = require("./db/db");
 const foods = getMenu();
 
 const tele = window.Telegram.WebApp;
 
 function App() {
-  const[cartItems,setCartItems] = useState([])
+  const [cartItems, setCartItems] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     tele.ready();
-  })
+  });
 
-  const onAdd = (food)=>{
-    const exist = cartItems.find(x=>x.id === food.id);
-    if (exist){
+  const onAdd = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist) {
       setCartItems(
-        cartItems.map((x)=>
-        x.id === food.id ? {...exist,quantity: exist.quantity + 1} : x
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
         )
       );
-    } else{
-      setCartItems([...cartItems,{...food, quantity:1}]);
+    } else {
+      setCartItems([...cartItems, { ...food, quantity: 1 }]);
     }
   };
 
   const onRemove = (food) => {
-    const exist = cartItems.find(x=>x.id === food.id);
-    if (exist.quantity===1){
-      setCartItems(cartItems.filter(x=>x.id !== food.id));
-    }else{
-      setCartItems(cartItems.map(x=>
-        x.id === food.id ? {...exist, quantity: exist.quantity - 1} : x))
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist.quantity === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== food.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+        )
+      );
     }
-  }
+  };
 
-  const onCheckout = ()=>{
-    tele.MainButton.text = "Confirm Order"
+  const onCheckout = () => {
+    tele.MainButton.text = "Confirm Order";
     tele.MainButton.show();
-  }
+  };
   return (
     <>
-    <h1 className="heading">Start Order!</h1>
-    <Cart cartItems={cartItems} onCheckout={onCheckout}/>
-    <div className="cards__container">
-    {foods.map(food => {
-      return <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove}/>
-    })}
-    </div>
+      <h1 className="heading">PayLeh! order</h1>
+      <Cart cartItems={cartItems} onCheckout={onCheckout} />
+      <div className="cards__container">
+        {foods.map((food) => {
+          return (
+            <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
+          );
+        })}
+      </div>
     </>
   );
 }
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,13 +3,13 @@ import './Cart.css'
 import Button from '../Button/Button';
 
 function Cart({cartItems, onCheckout}) {
-    const totalPrice = cartItems.reduce((a,c)=>a + c.price * c.quantity,0);
+    const totalPrice = cartItems.reduce((sum,item)=>sum + item.price * item.quantity,0);
   return (
   <div className="cart__container">
-      {cartItems.length === 0 ? "No items ordered " : ""}
+      {cartItems.length === 0 ? "No items ordered " : ""} {"   "}
       <br/> <span className="">Total Price: ${totalPrice.toFixed(2)}</span>
       <Button
-        title={`${cartItems.length === 0 ? "Order !" : "Checkout"} `}
+        title={`${cartItems.length === 0 ? "Order" : "Confirm order"} `}
         type={"checkout"}
         disable={cartItems.length === 0 ? true : false}
         onClick={onCheckout}
