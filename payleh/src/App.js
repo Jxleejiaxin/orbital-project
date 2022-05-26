8570b3e1f3c2b1358df3572ce764d51572aece94
Lee Jia Xin <jxleejiaxin@gmail.com>
2022-05-26 16:55:07 +0800

    all files

--- payleh/src/App.js
+++ payleh/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button';
+import Card from './components/Card/Card';
+const {getMenu} = require("./db/db");
+
+const foods = getMenu();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    Im here!
+    <Button title={'Add'} disable={false} type={'add'}/>
+    <Button title={'Remove'} disable={false} type={'remove'}/>
+    <Button title={'Checkout'} disable={false} type={'checkout'}/>
+    {foods.map(food => {
+      return <Card food={food} key={food.id}/>
+    })}
+    </>
   );
 }
 
