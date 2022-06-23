a6541a7272542ff8b161d352504c4e455b1459c8
giffordlee <giffordlee20@gmail.com>
2022-06-23 21:25:45 +0800

    some updates

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
-import { useState } from "react";
-=======
 import { useState, useEffect } from "react";
 import { Image } from "react-bootstrap";
->>>>>>> efc78956d1ff0cdec9b2f2b77bb96104dccab591
 import Cards from "./Cards/Cards.jsx";
 import Cart from "./Cart/Cart.jsx";
 import app from "../firebase.js";
@@ -90,7 +86,9 @@ export default function Menu() {
 
   return (
     <>
-<<<<<<< HEAD
+      <p>
+        <Image src="/logo.png" alt="" width="200" className="rounded mx-auto d-block"/>
+      </p>
       <label>
         Token:
         <input
@@ -102,22 +100,6 @@ export default function Menu() {
         />
         <h1>{OTP}</h1>
         <button onClick={onClick}>Click</button>
-=======
-      <p>
-        <Image src="/logo.png" alt="" width="200" className="rounded mx-auto d-block"/>
-      </p>
-      <label>Token: 
-      <input 
-      
-      type="text" 
-      name="otp" 
-      value={OTP} 
-      onChange={(e) => setOTP(e.target.value)}
-      placeholder="Input Token"
-      />
-      <h1>{OTP}</h1>
-      <button onClick={onClick}>Click</button>
->>>>>>> efc78956d1ff0cdec9b2f2b77bb96104dccab591
       </label>
 
 
