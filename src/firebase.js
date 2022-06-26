74c8545bc024fcd8c5b13942ff0dd55feb875da3
Jxleejiaxin <88620377+Jxleejiaxin@users.noreply.github.com>
2022-06-26 19:41:17 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyC2tgDG8OG9z8ylH4aM41qt9IhKJGOPXB0",
-    authDomain: "payleh-login-d6c39.firebaseapp.com",
-    projectId: "payleh-login-d6c39",
-    storageBucket: "payleh-login-d6c39.appspot.com",
-    messagingSenderId: "710816734570",
-    appId: "1:710816734570:web:f0a378d6636771875a1913"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 })
 
 export const auth = app.auth()
