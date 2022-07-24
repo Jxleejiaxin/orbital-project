fbfc35f26cbfaad486f08fe6f59878ba8e3eb256
Lee Jia Xin <jxleejiaxin@gmail.com>
2022-07-24 14:10:13 +0800

    Added environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyC2tgDG8OG9z8ylH4aM41qt9IhKJGOPXB0",
-    authDomain: "payleh-login-d6c39.firebaseapp.com",
-    projectId: "payleh-login-d6c39",
-    storageBucket: "payleh-login-d6c39.appspot.com",
-    messagingSenderId: "710816734570",
-    appId: "1:710816734570:web:f0a378d6636771875a1913"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MSG_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
 })
 
 export const auth = app.auth()
